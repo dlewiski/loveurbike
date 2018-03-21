7250c9b2d5d42b0ceff2596a45694b82f69d01d3
David Lewis and Kayla Ondracek <kayla@>
2018-03-21 11:18:32 -0700

    successfully returns colors and stolen location of each bike

--- src/bike.js
+++ src/bike.js
@@ -1,10 +1,10 @@
 export function bikeCall() {
   let promise = new Promise(function(resolve, reject) {
     let request = new XMLHttpRequest();
-    let location = 97227;
+    let location = "Portland, OR";
     let getElements;
     let proximity = 20;
-    let url = `https://bikeindex.org/api/v3/search?/api_key=${process.env.API_KEY}&location=${location}&stolenness=proximity&distance=${proximity}`;
+    let url = `https://bikeindex.org/api/v3/search?/api_key=${process.env.API_KEY}&location=${location}&stolenness=proximity&distance=${proximity}&per_page=100`;
 
     request.onload = function() {
       if (this.status === 200) {
@@ -22,9 +22,15 @@ export function bikeCall() {
   promise.then(function(response) {
     let body = JSON.parse(response);
     let simpleBikeArray = [];
-    console.log(body.bikes[0].title);
     body.bikes.forEach(function(element) {
-      simpleBikeArray.push(element.title);
+      let bikeColors = element.frame_colors[0];
+      if (element.frame_colors.length > 1) {
+        for (var i = 1; i < element.frame_colors.length; i++) {
+          bikeColors = bikeColors + ", " + element.frame_colors[i];
+        }
+      }
+      let bikeString = element.title + ", " + bikeColors + ", " + element.stolen_location;
+      simpleBikeArray.push(bikeString);
     });
 
     simpleBikeArray.forEach(function(element) {
